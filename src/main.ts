0d4ebd3e5f45946dda6c5bef772b81bcc00ccf01
Leonard Göhrs <leonard@goehrs.eu>
2024-10-03 00:01:41 +0200

    action: write upload-artifact implementation

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,95 @@
+import { relative } from 'node:path'
+import { open, rm, writeFile } from 'node:fs/promises'
+
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { HttpClient } from '@actions/http-client'
+import { findFilesToUpload } from './search'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+const FORREST_HOST = 'http://10.0.2.4'
+
+export interface UploadInputs {
+  artifactName: string
+  searchPath: string
+  includeHiddenFiles: boolean
+  authToken: string
+}
+
+export function getInputs(): UploadInputs {
+  const name = core.getInput('name') || 'artifact'
+  const path = core.getInput('path', { required: true })
+  const includeHiddenFiles = core.getBooleanInput('include-hidden-files')
+  const authToken = core.getInput('token')
+
+  return {
+    artifactName: name,
+    searchPath: path,
+    includeHiddenFiles: includeHiddenFiles,
+    authToken: authToken
+  }
+}
+
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
+  const inputs = getInputs()
+  const searchResult = await findFilesToUpload(
+    inputs.searchPath,
+    inputs.includeHiddenFiles
+  )
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+  let headers = undefined
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  if (inputs.authToken !== '') {
+    const bearer = `Bearer ${inputs.authToken}`
+    headers = { authorization: bearer }
+  }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+  const http = new HttpClient('forrest-upload-artifact/0.1', [], {
+    keepAlive: true,
+    headers: headers
+  })
+
+  const name = inputs.artifactName
+  const root = searchResult.rootDirectory
+
+  for (const path of searchResult.filesToUpload) {
+    const relativePath = relative(root, path)
+    const dstUrl = `${FORREST_HOST}/upload/${name}/${relativePath}`
+
+    core.info(`Uploading ${relativePath}`)
+    core.debug(`  - Destination: ${dstUrl}`)
+
+    const fd = await open(path)
+    const stream = fd.createReadStream()
+    const resp = await http.sendStream('PUT', dstUrl, stream)
+
+    const status = resp.message.statusCode
+    const message = resp.message.statusMessage
+    const sm = `${status} ${message}`
+
+    core.debug(`  - Status: "${sm}"`)
+
+    if (status != 201) {
+      core.setFailed(`Server did not respond with "201 Created" but "${sm}"`)
+      return
+    }
+
+    const publicUrl = resp.message.headers['content-location']
+
+    core.debug(`  - Public URL: "${publicUrl}"`)
+
+    if (publicUrl === undefined) {
+      core.setFailed('Server did not provide a public URL in response')
+      return
+    }
+
+    const desktopFilePath = path + `.desktop`
+    const desktopFileContent = `[Desktop Entry]\nType=Link\nURL=${publicUrl}`
+
+    await writeFile(desktopFilePath, desktopFileContent)
+
+    core.debug(`  - Created: "${desktopFilePath}"`)
+
+    await rm(path)
+
+    core.debug(`  - Removed: "${path}"`)
   }
 }
